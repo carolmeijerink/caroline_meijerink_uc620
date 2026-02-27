2dd423a09bacdb6560a28bcc21f687113ac77a9f
Caroline Meijerink <carolcris444@gmail.com>
2026-02-27 23:02:02 +0000

    add exe1

--- Aula1/aula.py
+++ Aula1/aula.py
@@ -1,1 +1,23 @@
-Print ("Hello, world!");
+
+    
+val1=2
+val2=3
+val3=4
+
+if val1>val2 and val2>val3:
+    print("val1 é o maior e val3 é o menor.")
+
+elif val2>val1 and val1>val3:
+    print("val2 é o maior e val3 é o menor.")
+
+elif val3>val1 and val1>val2:
+    print("val3 é o maior e val2 é o menor.")
+
+elif val1>val3 and val3>val2:
+    print("val1 é o maior e val2 é o menor.")
+
+elif val3>val2 and val2>val1:
+    print("val3 é o maior e val1 é o menor.")
+
+elif val2>val3 and val3>val1:
+    print("val2 é o maior e val1 é o menor.")
