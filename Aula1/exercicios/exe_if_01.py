fa8ac8754ce62eb88e839f4616400ad5d5f310d4
Caroline Meijerink <carolcris444@gmail.com>
2026-03-03 17:40:02 +0000

    add exe_if_01

---
+++ Aula1/exercicios/exe_if_01.py
@@ -0,0 +1,16 @@
+
+
+
+print("Qual valor, em segundos, pretendes converter?")
+
+seg=int(input())
+
+horas= seg // 3600
+
+seg= seg % 3600
+
+minutos= seg // 60
+
+seg = seg % 60
+
+print(f"O valor convertido é igual a {horas} horas, {minutos} miutos e {seg} segundos.")
