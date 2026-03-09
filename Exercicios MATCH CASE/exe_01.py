c92a9e98c828ac88d16e9260efe3dbfc715b8c2f
Caroline Meijerink <carolcris444@gmail.com>
2026-03-09 15:05:34 +0000

    exe_01 - input dia from int to string

--- Exercicios MATCH CASE/exe_01.py
+++ Exercicios MATCH CASE/exe_01.py
@@ -1,26 +1,26 @@
 
-dia = int(input("Qual dia da semana queres verificar se é útil ou não? Escolha do menu abaixo, utilizando o número correspondente (ex. prima 1 para segunda-feira):\n1 - Segunda-feira;\n2 - Terça-feira; \n3 - Quarta-feira; \n4 - Quinta-feira; \n5 - Sexta-feira; \n6 - Sábado; \n7 - Domingo;\n\nR: "))
+dia = input("Qual dia da semana queres verificar se é útil ou não? Escolha do menu abaixo, utilizando o número correspondente (ex. prima 1 para segunda-feira):\n1 - Segunda-feira;\n2 - Terça-feira; \n3 - Quarta-feira; \n4 - Quinta-feira; \n5 - Sexta-feira; \n6 - Sábado; \n7 - Domingo;\n\nR: ")
 
 match dia:
-    case 1:
+    case "1":
         print("A segunda-feira é um dia útil.")
 
-    case 2:
+    case "2":
         print("A terça-feira é um dia útil.")
 
-    case 3:
+    case "3":
         print("A quarta-feira é um dia útil.")
 
-    case 4:
+    case "4":
         print("A quinta-feira é um dia útil.")
 
-    case 5:
+    case "5":
         print("A sexta-feira é um dia útil.")
 
-    case 6:
+    case "6":
         print("O sábado é final de semana.")
 
-    case 7:
+    case "7":
         print("O domingo é final de semana.")
 
     case _:
