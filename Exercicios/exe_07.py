e6185edfb563367f5abd01e730762c369939d499
Caroline Meijerink <carolcris444@gmail.com>
2026-03-09 14:16:04 +0000

    add exe07

---
+++ Exercicios/exe_07.py
@@ -0,0 +1,16 @@
+
+
+nota1 = 10
+nota2 = 2
+nota3 = 6
+
+media = (nota1*0.20) + (nota2*0.30) + (nota3*0.50)
+
+
+if media>=6:
+    print(f"Média: {media}\nAPROVADO.")
+
+
+elif media<6:
+    print(f"Média: {media}\nREPROVADO.")
+
