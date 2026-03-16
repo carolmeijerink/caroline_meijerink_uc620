2c6687c23a10885998ea98bc49356cb08c8811f2
Caroline Meijerink <carolcris444@gmail.com>
2026-03-16 22:44:30 +0000

    add exe19

--- Exercícios/Exercícios LOOP/exe_19.py
+++ Exercícios/Exercícios LOOP/exe_19.py
@@ -1,4 +1,17 @@
 
 
+## este já consegui sem IA! =D yayy
 
+fibo = [0] * 60
 
+fibo[0] = 0
+fibo[1] = 1
+
+for i in range(2, len(fibo)):
+    fibo[i] = (fibo[i-1]) + fibo[i-2]
+
+    
+
+print(fibo)
+
+    
